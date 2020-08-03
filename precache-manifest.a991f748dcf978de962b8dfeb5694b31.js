bfbcfe51d9c42d7cbc6d31d1de6a89124e788939
Petros <petrospetrosyanjs.96@gmail.com>
2020-08-04 02:53:46 +0400

    Updates

---
+++ precache-manifest.a991f748dcf978de962b8dfeb5694b31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39ef8630b883343cb753fbb449364032",
+    "url": "/x_or_o/index.html"
+  },
+  {
+    "revision": "0d1ea3b1cde8989fdb5f",
+    "url": "/x_or_o/static/css/main.34eb9399.chunk.css"
+  },
+  {
+    "revision": "869c8459f7c40e31552f",
+    "url": "/x_or_o/static/js/2.8dd71c4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/x_or_o/static/js/2.8dd71c4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d1ea3b1cde8989fdb5f",
+    "url": "/x_or_o/static/js/main.4029d0b6.chunk.js"
+  },
+  {
+    "revision": "383f02456c99497d46ff",
+    "url": "/x_or_o/static/js/runtime-main.c2aba5c1.js"
+  }
+]);
